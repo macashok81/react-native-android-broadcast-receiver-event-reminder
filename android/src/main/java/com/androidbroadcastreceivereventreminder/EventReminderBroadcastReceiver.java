4d6652a4bbdccfd0e329de34506efdb43492c679
macashok81 <ashok.kr.03@gmail.com>
2020-07-07 15:56:07 +0530

    change

--- android/src/main/java/com/androidbroadcastreceivereventreminder/EventReminderBroadcastReceiver.java
+++ android/src/main/java/com/androidbroadcastreceivereventreminder/EventReminderBroadcastReceiver.java
@@ -8,50 +8,30 @@ import android.util.Log;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-import android.net.Uri;
-import android.provider.CalendarContract;
-import android.database.Cursor;
-
 /**
  * Created by freedomson on 08-01-2018.
  */
 
 public class EventReminderBroadcastReceiver extends BroadcastReceiver {
 
-    public static final String EVENT_ACTION = "android.intent.action.EVENT_REMINDER";
+    //public static final String EVENT_ACTION = "android.intent.action.EVENT_REMINDER";
+    public static final String EVENT_ACTION = "android.intent.action.PACKAGE_FULLY_REMOVED";
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Log.d("ReactNativeJS", "EventReminderBroadcastReceiver event fired: " + intent.getAction());
+        Log.d("ReactNativeJS111", "EventReminderBroadcastReceiver event fired: " + intent.getAction());
 
         if (!EVENT_ACTION.equals(intent.getAction())) {
             return;
         }
-
-        Uri uri = intent.getData();
-
-        String alertTime = uri.getLastPathSegment();
-
-        String selection = CalendarContract.CalendarAlerts.ALARM_TIME + "=?";
-
-        Cursor cursor = context.getContentResolver().query(
-            CalendarContract.CalendarAlerts.CONTENT_URI_BY_INSTANCE
-            , new String[]{CalendarContract.CalendarAlerts.EVENT_ID}
-            , selection,new String[]{alertTime}
-            , null);
-        
-        if ( cursor.moveToNext() ) {
-            Integer eventId = cursor.getInt(0);
-            Log.d("ReactNativeJS", "EventReminderBroadcastReceiver eventid: " + eventId);
-            sendEvent("GEventReminderBroadcastReceiver", eventId);
-        } else {
-            Log.d("ReactNativeJS", "EventReminderBroadcastReceiver no id found");
-        }
+        Log.v("ReactNativeJS111", "EventReminderBroadcastReceiver event fired: " + intent.getData());
+        sendEvent("GEventReminderBroadcastReceiver", intent.getData().toString());
 
     }
 
-    private void sendEvent(String eventName, Integer eventId) {
+    private void sendEvent(String eventName, String eventId) {
         try{
             ReactContext reactContext = RNAndroidBroadcastReceiverEventReminderModule.reactContext;
 
